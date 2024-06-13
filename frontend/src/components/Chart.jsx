5fdfd1d3a1a9800c684c3fe1cc07e459e43b329e
dbs1507 <danielbloch15@gmail.com>
2024-06-13 09:24:48 -0300

    commit teste

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -6,7 +6,7 @@ import { useRef, useEffect } from 'react';
 
 export default function ChartComponent(){
   <div>
-    <h2>Bar  (custom size)</h2>
+    <h2>Bar Example (custom size)</h2>
     <Bar/>
   </div>
 
