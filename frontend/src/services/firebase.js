1cde2235b572a4827f8ec2f58fdd7f3eb433efd4
dbs1507 <danielbloch15@gmail.com>
2024-07-01 11:41:49 -0300

    credencials .env

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -3,15 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDqrhI8odGogD7FTJvhUWVlYGqeSlYE3pg",
-    authDomain: "papersfinder-aed7c.firebaseapp.com",
-    projectId: "papersfinder-aed7c",
-    storageBucket: "papersfinder-aed7c.appspot.com",
-    messagingSenderId: "671486215155",
-    appId: "1:671486215155:web:5a336c414e1a8e505ec886",
-    measurementId: "G-613HL8MP3V"
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
-    
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
