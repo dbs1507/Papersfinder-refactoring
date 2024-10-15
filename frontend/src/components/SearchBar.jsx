53b30180ed51246e3e67cdeb8744be98a29592e5
dbs1507 <danielbloch15@gmail.com>
2024-10-15 11:23:06 -0300

    new

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -15,13 +15,13 @@ const SearchBar = () => {
       setAlert("Pesquisa vazia, digite alguma palavra");
       return;
     }
-  
+
     setLoading(true); // Ativa o loading
-  
+
     try {
       const response = await fetch(`/api/combinedSearch?query=${encodeURIComponent(query)}&page=0`);
       const data = await response.json();
-  
+
       if (!data.trendsData) {
         setAlert("Não há dados de pesquisa suficientes para exibir o gráfico de interesse");
       } else {
@@ -31,10 +31,10 @@ const SearchBar = () => {
           trendsData: data.trendsData,
           query,
         });
-  
+
         // Salva a pesquisa no localStorage
         localStorage.setItem('lastSearch', JSON.stringify({ googleData: data.googleData, trendsData: data.trendsData, query }));
-  
+
         // Navega para a página /result com parâmetros de consulta
         router.push(`/result?query=${encodeURIComponent(query)}&page=0`);
       }
@@ -45,7 +45,7 @@ const SearchBar = () => {
       setLoading(false); // Desativa o loading após a conclusão da busca
     }
   };
-  
+
   // Adiciona o manipulador para detectar a tecla Enter
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
@@ -78,12 +78,12 @@ const SearchBar = () => {
         </button>
 
       </div>
-        <div class="relative group pt-32">
-            <span class="text-gray-500 cursor-pointer text-sm">Como funciona ?</span>
-            <div class="absolute left-1/2 transform -translate-x-1/2 -translate-y-10 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 bg-gray-900 text-white text-sm rounded-lg shadow-lg p-2 w-64 z-10">
-            Digite uma palavra-chave relacionada ao tema de interesse, clique em 'Buscar' e obtenha estatísticas com base no Google Trends, além de sugestões de artigos vindas diretamente do Google Scholar para aprofundar seus conhecimentos sobre o assunto pesquisado.
-            </div>
+      <div className="relative group pt-32">
+        <span className="text-gray-500 cursor-pointer text-sm">Como funciona ?</span>
+        <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-10 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 bg-gray-900 text-white text-sm rounded-lg shadow-lg p-2 w-64 z-10">
+          Digite uma palavra-chave relacionada ao tema de interesse, clique em &apos;Buscar&apose; e obtenha estatísticas com base no Google Trends, além de sugestões de artigos vindas diretamente do Google Scholar para aprofundar seus conhecimentos sobre o assunto pesquisado.
         </div>
+      </div>
       {alert && (
         <div className="mt-5 text-red-500">
           {alert}
